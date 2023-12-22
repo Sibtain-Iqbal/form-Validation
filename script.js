1beec715f8553cf6adcb027f94306087aac1c4e5
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2023-12-22 07:06:40 -0800

    form js

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ const form = document.querySelector("form"),
   passInput = passField.querySelector(".password"),
   cPassField = form.querySelector(".confirm-password"),
   cPassInput = cPassField.querySelector(".cPassword");
+  function checkEmail() {
+    const emaiPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (!emailInput.value.match(emaiPattern)) {
+      return emailField.classList.add("invalid"); //adding invalid class if email value do not mathced with email pattern
+    }
+    emailField.classList.remove("invalid"); //removing invalid class if email value matched with emaiPattern
+  }
+  
+  // Hide and show password
+  const eyeIcons = document.querySelectorAll(".show-hide");
+  
+  eyeIcons.forEach((eyeIcon) => {
+    eyeIcon.addEventListener("click", () => {
+      const pInput = eyeIcon.parentElement.querySelector("input"); //getting parent element of eye icon and selecting the password input
+      if (pInput.type === "password") {
+        eyeIcon.classList.replace("bx-hide", "bx-show");
+        return (pInput.type = "text");
+      }
+      eyeIcon.classList.replace("bx-show", "bx-hide");
+      pInput.type = "password";
+    });
+  });
